5c86a6ba71f13c26f31f1c0592733b8b56e8beea
Dominic Lind <linddominic@gmail.com>
2017-02-19 15:30:24 +0100

    + fix if workout doesnt have any sets

--- app/components/WorkoutInfoModalContent/index.js
+++ app/components/WorkoutInfoModalContent/index.js
@@ -30,12 +30,15 @@ class WorkoutInfoModalContent extends Component {
       <View style={ styles.component }>
         <Header style={{fontSize: 22, marginBottom: 30}}>Workout Summary</Header>
         {exercises.reverse().map((exercise, i) => {
-          const sets = exercise.sets.map((set, i) => {
-            return <Paragraph style={{fontSize: 14}}>{set.reps}x{set.weight}kg{i == exercise.sets.length-1 ? '' : ', '}</Paragraph>;
-          });
+          let sets = [];
+          if (exercise.sets) {
+            sets = exercise.sets.map((set, i) => {
+              return <Paragraph key={i} style={{fontSize: 14}}>{set.reps ? set.reps : 0}x{set.weight ? set.weight : 0}kg{i == exercise.sets.length-1 ? '' : ', '}</Paragraph>;
+            });
+          }
 
           return (
-            <View>
+            <View key={i}>
               <Paragraph weight="bold" style={{marginBottom:5}} key={i}>{exercise.name}</Paragraph>
               <View style={{flexDirection:'row'}}>{sets}</View>
             </View>
